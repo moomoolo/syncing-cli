df936e5a72f8dd0fe21128f6304aa553216b712c
moomoolo <772419236@qq.com>
2022-10-02 01:18:48 +0800

    feat: add formatTime function

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -5,10 +5,20 @@ export function toErrorStr(msg: string) {
   return `${chalk.red('ERROR')} ${msg}`;
 }
 
+export function formatTime(date: Date) {
+  let res = `${date.toLocaleString('en-US', { year: 'numeric' })}-`;
+  res += `${date.toLocaleString('en-US', { month: '2-digit' })}-`;
+  res += `${date.toLocaleString('en-US', { day: '2-digit' })} `;
+  res += `${date.toLocaleString('en-US', { hour: '2-digit', hourCycle: 'h24' })}:`;
+  res += `${date.toLocaleString('en-US', { minute: '2-digit' })}:`;
+  res += date.toLocaleString('en-US', { second: '2-digit' });
+  return res;
+}
+
 export function appendModifyTime(dirList: string[]) {
   return dirList.map((dir) => {
     const modifyTime = new Date(lstatSync(dir).mtime);
-    const coloredModifiTime = chalk.grey(`(last modify: ${modifyTime.toLocaleString()})`);
+    const coloredModifiTime = chalk.grey(`(last modify: ${formatTime(modifyTime)})`);
     return `${dir} ${coloredModifiTime}`;
   });
 }
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,35 +1,36 @@
 import { red, blue, yellow, green } from './colors';
+import { formatTime } from './formatter';
 
 const log = console.log;
 
 export function logError(msg: string) {
-  log(`${new Date().toLocaleString()} |${red('ERROR  ')}| ${msg}`);
+  log(`${formatTime(new Date())} |${red('ERROR  ')}| ${msg}`);
 }
 
 export function logChange(name: string, parent: string, type: 'file' | 'directory') {
-  let res = `${new Date().toLocaleString()} |${blue('CHANGE ')}| `;
+  let res = `${formatTime(new Date())} |${blue('CHANGE ')}| `;
   res += `${type === 'file' ? '📃' : '📂'} ${name} in ${blue(parent)}`;
   log(res);
 }
 
 export function logAddDir(name: string, parent: string) {
-  log(`${new Date().toLocaleString()} |${green('ADDDIR ')}| 📂 ${name} in ${blue(parent)}`);
+  log(`${formatTime(new Date())} |${green('ADDDIR ')}| 📂 ${name} in ${blue(parent)}`);
 }
 
 export function logAddFile(name: string, parent: string) {
-  log(`${new Date().toLocaleString()} |${green('ADDFILE')}| 📃 ${name} in ${blue(parent)}`);
+  log(`${formatTime(new Date())} |${green('ADDFILE')}| 📃 ${name} in ${blue(parent)}`);
 }
 
 export function logDeleteDir(name: string, parent: string) {
-  log(`${new Date().toLocaleString()} |${red('DELDIR ')}| 📂 ${name} in ${blue(parent)}`);
+  log(`${formatTime(new Date())} |${red('DELDIR ')}| 📂 ${name} in ${blue(parent)}`);
 }
 
 export function logDeleteFile(name: string, parent: string) {
-  log(`${new Date().toLocaleString()} |${red('DELFILE')}| 📃 ${name} in ${blue(parent)}`);
+  log(`${formatTime(new Date())} |${red('DELFILE')}| 📃 ${name} in ${blue(parent)}`);
 }
 
 export function logSync(name: string, parent: string, type: 'file' | 'directory') {
-  let res = `${new Date().toLocaleString()} |${yellow('SYNC   ')}| `;
+  let res = `${formatTime(new Date())} |${yellow('SYNC   ')}| `;
   res += `${type === 'file' ? '📃' : '📂'} ${name} to ${blue(parent)}`;
   log(res);
 }
