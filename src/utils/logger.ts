25df47bf633bd3824d50fe6203d0448c5cf46348
moomoolo <772419236@qq.com>
2022-10-03 21:24:17 +0800

    feat: change blue to cyan in wathc log

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,4 +1,4 @@
-import { red, blue, yellow, green } from './colors';
+import { red, yellow, green, cyan } from './colors';
 import { formatTime } from './formatter';
 
 const log = console.log;
@@ -8,29 +8,29 @@ export function logError(msg: string) {
 }
 
 export function logChange(name: string, parent: string, type: 'file' | 'directory') {
-  let res = `${formatTime(new Date())} |${blue('CHANGE ')}| `;
-  res += `${type === 'file' ? '📃' : '📂'} ${name} in ${blue(parent)}`;
+  let res = `${formatTime(new Date())} |${cyan('CHANGE ')}| `;
+  res += `${type === 'file' ? '📃' : '📂'} ${name} in ${cyan(parent)}`;
   log(res);
 }
 
 export function logAddDir(name: string, parent: string) {
-  log(`${formatTime(new Date())} |${green('ADDDIR ')}| 📂 ${name} in ${blue(parent)}`);
+  log(`${formatTime(new Date())} |${green('ADDDIR ')}| 📂 ${name} in ${cyan(parent)}`);
 }
 
 export function logAddFile(name: string, parent: string) {
-  log(`${formatTime(new Date())} |${green('ADDFILE')}| 📃 ${name} in ${blue(parent)}`);
+  log(`${formatTime(new Date())} |${green('ADDFILE')}| 📃 ${name} in ${cyan(parent)}`);
 }
 
 export function logDeleteDir(name: string, parent: string) {
-  log(`${formatTime(new Date())} |${red('DELDIR ')}| 📂 ${name} in ${blue(parent)}`);
+  log(`${formatTime(new Date())} |${red('DELDIR ')}| 📂 ${name} in ${cyan(parent)}`);
 }
 
 export function logDeleteFile(name: string, parent: string) {
-  log(`${formatTime(new Date())} |${red('DELFILE')}| 📃 ${name} in ${blue(parent)}`);
+  log(`${formatTime(new Date())} |${red('DELFILE')}| 📃 ${name} in ${cyan(parent)}`);
 }
 
 export function logSync(name: string, parent: string, type: 'file' | 'directory') {
   let res = `${formatTime(new Date())} |${yellow('SYNC   ')}| `;
-  res += `${type === 'file' ? '📃' : '📂'} ${name} to ${blue(parent)}`;
+  res += `${type === 'file' ? '📃' : '📂'} ${name} to ${cyan(parent)}`;
   log(res);
 }
